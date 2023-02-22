856dade221d0abde179a27116a6d242df820353e
AKA-Pierre <gianpiero416@icloud.com>
2023-02-22 19:29:05 +1100

    new commit

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+// Create email for employees
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email em1 = new Email("John", "Smith");
+        System.out.println(em1.showInfo());
+
+    }
+}
